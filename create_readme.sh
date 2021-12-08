c0a62def81b4d404bae9e9aef5dbb378ce6fffa0
Dave Tang <davetingpongtang@gmail.com>
2021-12-08 13:34:09 +0900

    Include MkDocs step

--- create_readme.sh
+++ create_readme.sh
@@ -2,12 +2,11 @@
 
 set -euo pipefail
 
-export PATH=$PATH:miniconda3/bin/
-
 out_md=tmp.md
 Rscript -e "rmarkdown::render('learning_bam_file.Rmd', output_file=\"$out_md\")"
 github-markdown-toc/gh-md-toc $out_md > toc
 
+cp -f $out_md mkdocs/docs/index.md
 cat toc <(echo) <(date) <(echo) $out_md > README.md
 
 rm $out_md toc
